ab0d52cd58cac58c8535a4cc0aee0f0b54506285
Evan St. Paul <evanstpaul16@augustana.edu>
2020-04-06 14:36:03 -0500

    Added navigator.geolocation support to get current location from user

--- src/App.js
+++ src/App.js
@@ -6,16 +6,29 @@ import Axios from 'axios';
 const openWeatherApiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
 
 const App = () => {
-    const [ selectedCity, setSelectedCity ] = useState('');
+    // data from google places api
+    const [ selectedCity, setSelectedCity ] = useState('initial');
     const [ selectedLong, setSelectedLong ] = useState('');
     const [ selectedLat, setSelectedLat ] = useState('');
+
+    // data from openWeatherApi
     const [ currentTemp, setCurrentTemp ] = useState('');
     const [ realFeel, setRealFeel ] = useState('');
     const [ minTemp, setMinTemp ] = useState('');
     const [ maxTemp, setMaxTemp ] = useState('');
     const [ weatherDescription, setWeatherDiscription ] = useState('');
-    const [ forecast, setForecast ] = useState([]);
+    const [ forecast, setForecast ] = useState([]); // for later
     const [ isInitialRender, setIsInitialRender ] = useState(true);
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                setSelectedLong(position.coords.longitude);
+                setSelectedLat(position.coords.latitude);
+                setIsInitialRender(false);
+            });
+        }
+    }, []);
 
     useEffect(
         () => {
@@ -32,9 +45,12 @@ const App = () => {
                 setRealFeel(kelvinsToFahrenheit(temps.feels_like));
                 setMinTemp(kelvinsToFahrenheit(temps.temp_min));
                 setMaxTemp(kelvinsToFahrenheit(temps.temp_max));
+                if (selectedCity === 'initial') {
+                    setSelectedCity(response.data.name);
+                }
             });
         },
-        // These are the last to be updated, so rerender after them
+        // These are the last to be updated from updateCity, so rerender after them
         [ selectedLong, isInitialRender ]
     );
 
@@ -51,17 +67,16 @@ const App = () => {
         return Math.round(fahr);
     };
 
-    const getWeather = () => {
-        Axios.get(
-            `http://api.openweathermap.org/data/2.5/weather?lat=${selectedLat}&lon=${selectedLong}&appid=${openWeatherApiKey}`
-        ).then((response) => {
-            console.log(response);
-        });
-    };
-
     return (
         <div className='App'>
-            <Weather />
+            <Weather
+                city={selectedCity}
+                temp={currentTemp}
+                minTemp={minTemp}
+                maxTemp={maxTemp}
+                realFeel={realFeel}
+                description={weatherDescription}
+            />
             <FindLocation updateCity={updateCity} />
         </div>
     );
